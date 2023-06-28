5758fc0256aae3c9b6d1f95658485fddafa7ffab
Dan Murphy <danmurph1994@gmail.com>
2023-06-28 17:56:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,20 @@ import altair as alt
 import pandas as pd
 import plotly.express as px
 
+
 def main():
     df = pd.read_csv(st.file_uploader("Please upload a file for analysis",type=['csv'], accept_multiple_files=False,))
+    
     page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
-
     if page == "Homepage":
         st.header("This is your data explorer.")
         st.write("Please select a page on the left.")
         st.write(df)
     elif page == "Exploration":
         st.title("Data Exploration")
-        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
-        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
-        print(x_axis)
-        fig = px.bar(df,x=x_axis,y=y_axis)
+        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns)
+        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns)
+        fig = px.scatter(df,x=x_axis,y=y_axis)
         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
