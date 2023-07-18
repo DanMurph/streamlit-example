2f2475e759b319780ac8a71c0c8c6f1f247296f7
Dan Murphy <danmurph1994@gmail.com>
2023-07-18 14:49:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,17 @@ def main():
     df = pd.read_csv(st.file_uploader("Please upload a file for analysis",type=['csv'], accept_multiple_files=False,))
     
     page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
+    
     if page == "Homepage":
         st.header("This is your data explorer.")
         st.write("Please select a page on the left.")
         st.write(df)
     elif page == "Exploration":
         st.title("Data Exploration")
+        chart_type = st.selectbox("Choose your chart type", ["scatter","line","bar"])
         x_axis = st.selectbox("Choose a variable for the x-axis", df.columns)
         y_axis = st.selectbox("Choose a variable for the y-axis", df.columns)
-        fig = px.scatter(df,x=x_axis,y=y_axis)
+        fig = px.chart_type(df,x=x_axis,y=y_axis)
         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
